d144556a703640b8ce56f68ad8a2b0d4e5fcd8ba
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 15:07:50 -0300

    atividade-lp-29

---
+++ exercicio29.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a sua primeira nota: "))
+nota2 = float(input("Digite a sua segunda nota: "))
+nota3 = float(input("Digite a sua terceira nota: "))
+nota4 = float(input("Digite a sua quarta nota: "))
+
+media = ((nota1 + nota2 + nota3 + nota4)/4)
+
+print("A média aritmética de suas notas é: {} ".format(media))

72f4d74d995b5e36c0516d7b017f827303d1b682
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 14:28:14 -0300

    Create exercicio03.py

---
+++ exercicio03.py
@@ -0,0 +1,10 @@
+altura = float(input("Digite a altura desejada da parede em metros: "))
+comprimento = float(input("Digite o comprimento desejado da parede em metros: "))
+
+area = (altura*comprimento)
+cobrimento = (area/3)
+latas = (cobrimento//3.6)
+total = (latas*107.00)
+
+print("Você terá que comprar {:.0f} latas de tinta que custarão {:.2f} reais.".format(latas, total))
+

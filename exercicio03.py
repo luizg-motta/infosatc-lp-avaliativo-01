09caac8a02830f6ac59a4c75c7917bcc846d9090
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 14:46:12 -0300

    Update exercicio03.py

--- exercicio03.py
+++ exercicio03.py
@@ -6,5 +6,6 @@ cobrimento = (area/3)
 latas = (cobrimento//3.6)
 total = (latas*107.00)
 
+
 print("Você terá que comprar um total de {:.0f} latas de tinta que custarão {:.2f} reais.".format(latas, total))
 

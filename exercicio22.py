4e9a6d0996c9a5f6ba6ff31522865b89852c6c2d
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 14:59:15 -0300

    atividade-lp-22

---
+++ exercicio22.py
@@ -0,0 +1,3 @@
+jardas = float(input("Digite um valor em jardas para ser convertido em metros: "))
+
+print("O valor em jardas {:.1f} convertido para metros é: {:.1f}".format(jardas, (0.91*jardas)))

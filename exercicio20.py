ddda8997e4ee935369e80aa42900f780586ee33b
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 14:57:20 -0300

    atividade-lp-20

---
+++ exercicio20.py
@@ -0,0 +1,3 @@
+massa = float(input("Digite um valor de massa em kg para ser convertido para libras: "))
+
+print("O valor da massa {:.1f} convertida para libras: {:.1f}".format(massa, (massa/0.45)))

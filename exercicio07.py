c37a09dc2abccbd373476a72e1da8aa83de530a2
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 14:47:21 -0300

    atividade-lp-07

--- exercicio07.py
+++ exercicio07.py
@@ -1,3 +1,4 @@
 f = float(input("Digite uma temperatura em graus Fahrenheit para ser convertida em Celsius: "))
 
+
 print("A temperatura {} em Fahrenheit convertida para Celsius é: {:.1f} Graus".format(f, (5.0*(f-32.0)/9)))

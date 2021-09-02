09ea9555d8ed7572fd6b1a14a4f130c93eb4416a
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-02 15:10:19 -0300

    atividade-lp-30

---
+++ exercicio30.py
@@ -0,0 +1,5 @@
+real = float(input("Digite o valor em real para converter em dolar: "))
+dolar = float(input("Digite a atual cotação do dolar: "))
+
+print("O valor de {} reais convertido em dolar resulta em {} ".format(real, (real/dolar)))
+
